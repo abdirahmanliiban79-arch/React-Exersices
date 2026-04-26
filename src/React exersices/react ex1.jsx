54e088348b28745ee153fa6aca48e626f88a3625
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-04-26 15:27:23 +0300

    submited ex1

---
+++ src/React exersices/react ex1.jsx
@@ -0,0 +1,9 @@
+const UseCard = ()=>{
+    return (
+        <div style={{textAlign:'center',fontSize:'30px'}}>
+        <h2>John Doe</h2>
+        <p>John Doe@gmail.com</p>
+        </div>
+    )
+}
+export default UseCard;
